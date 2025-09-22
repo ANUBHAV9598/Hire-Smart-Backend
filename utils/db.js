a7147f5cf7b7f56775b9431c6e803a91b862bdee
Anubhav Pandya <anubhavpandya99@gmail.com>
2025-09-22 17:01:51 +0530

    first commit

---
+++ utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export let isConnected = false;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
+        isConnected = true;
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log('Error in connecting to mongodb', error);
+    }
+}
+export default connectDB;
